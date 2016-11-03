9131347b2517b9393ff6dc02661149f2089ecce4
Benjamin Cisneros <benjamin cisneros@tiempodev.local>
2016-11-03 16:54:14 -0600

    Add more changes

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,14 @@
-
+/**
+ * Hello World 
+ */
 
 public class HolaMundo {
     public static void main(String... args) {
+        HolaMundo hello = new HolaMundo();
+	hello.sayHello();
+    }
+
+    void sayHello() {
         System.out.println("Hola Mundo!");
     }
 }
